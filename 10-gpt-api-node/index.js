ddff5bd8b9c036849213dd0393242c22d1021748
Alexandre Russi Jr <alexandrerussijunior@gmail.com>
2024-05-17 10:05:26 -0300

    finalizando api node openai chatgpt

--- 10-gpt-api-node/index.js
+++ 10-gpt-api-node/index.js
@@ -26,16 +26,42 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
-// CRIAR O PROJETO BACK-END A PARTIR DAQUI...
-
 const openai = new OpenAI({
     organization: "org-3W1346Cr1v3uVR3yDPiYb4XI",
-    apiKey: "sk-proj-LgpcbVzrYIzejPpooWmnT3BlbkFJ0Mc0HUYRvYF7Xjxj6d4H",
+    apiKey: "sk-proj-HluZNz1Np1fAjTwDfZzkT3BlbkFJdWC2NBCK2Y1ifT4Sgr9q",
 });
 
-const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": "Quem é você?"}]
-})
+// CRIAR O PROJETO BACK-END A PARTIR DAQUI...
 
-console.log(chatCompletion.choices[0].message);
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// CRIANDO ENDPOINT PARA ENVIAR MENSAGENS A API DO CHATGPT
+app.post("/sendMessage", async (req, res) => {
+    const { messages } = req.body; // é oq veio do meu front
+
+    // vc pode trocar pela API do Gemini - Google
+    const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {"role": "system", "content": "Você responderá todas as perguntas como se fosse o presidente da Rússia, Vladimir Putin"},
+            ...messages
+        ]
+    });
+    
+    console.log(chatCompletion.choices[0].message);
+
+    // é a resposta que vou retornar para o meu front-end
+    // isso só é possível pq to fazendo via metodo POST!!!
+    res.json({
+        chat_completion: chatCompletion.choices[0]
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor aberto executando em http://localhost:${port}/`);
+});
+
